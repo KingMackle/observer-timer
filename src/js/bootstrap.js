79343892fae37296d72bd0bfcc2f8350d30c1367
KingMackle <mahmoudttrk@gmail.com>
2018-06-24 01:27:25 -0400

    easy testing

--- src/js/bootstrap.js
+++ src/js/bootstrap.js
@@ -8,14 +8,14 @@ const timer = new TimerModel()
 let numberOfDisplays = 10000
 let observers = []
 
-for (let i = 1; i <= numberOfDisplays; i++) {
-  let displayTemplate = '<div class="display-wrapper">' +
-                        `<p class="display" id="display${i}"></p>` +
-                        '<input class="subscribe" type="button" value="Sub"/>' +
-                        '<input class="unsubscribe" type="button" value="Unsub"/>' +
-                        '</div>'
-  wrapper.innerHTML = wrapper.innerHTML.concat(displayTemplate)
-}
+// for (let i = 1; i <= numberOfDisplays; i++) {
+//   let displayTemplate = '<div class="display-wrapper">' +
+//                         `<p class="display" id="display${i}"></p>` +
+//                         '<input class="subscribe" type="button" value="Sub"/>' +
+//                         '<input class="unsubscribe" type="button" value="Unsub"/>' +
+//                         '</div>'
+//   wrapper.innerHTML = wrapper.innerHTML.concat(displayTemplate)
+// }
 
 for (let i = 1; i <= numberOfDisplays; i++) {
   observers.push(new Display(`display${i}`, timer))
